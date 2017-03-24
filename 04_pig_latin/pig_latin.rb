7c6f02ae2856add06c944b5dd2922eecc5c446d7
latsch <sbrossmer@googlemail.com>
2017-03-24 13:42:59 +0100

    Solved test problems with String manipulation test

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
-#write your code here
+def translate(s)
+	if s.match(" ")
+		splitWord = s.split(' ')
+		wordF = ''
+		splitWord.each do |word|
+			wordF << translate(word) + " "
+		end
+		s = wordF[0...-1] # Cutting the last whitespace
+		return s
+	end
+
+	if s.start_with?('a', 'e', 'i', 'o', 'u')
+		s << "ay"
+	else if s.start_with?('qu')
+		s = "#{s[2..-1]}#{s[0..1]}"
+		translate(s)
+	else
+		s = "#{s[1..-1]}#{s[0]}"
+		translate(s)
+	end
+	end
+end
